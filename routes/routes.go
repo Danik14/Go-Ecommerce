bc8ffed495f68e42cca35e3712d3c8d5ea791781
Danik <chapagana@gmail.com>
2022-11-30 16:05:59 +0600

    Added creating product by admin

--- routes/routes.go
+++ routes/routes.go
@@ -8,7 +8,7 @@ import (
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/users/signup", handlers.SignUp())
 	incomingRoutes.POST("/users/login", handlers.Login())
-	// incomingRoutes.POST("/admin/addproduct", handlers.ProductViewerAdmin())
+	incomingRoutes.POST("/admin/addproduct", handlers.AddProductAdmin())
 	// incomingRoutes.GET("/users/productview", handlers.SearchProduct())
 	// incomingRoutes.GET("/users/search", handlers.SearchProductByQuery())
 }
