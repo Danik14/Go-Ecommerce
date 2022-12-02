ef3facc9339b60c8b33cb264bc5f2c76d465fad7
Danik <chapagana@gmail.com>
2022-12-02 22:58:38 +0600

    Added removing item from cart

--- database/cart.go
+++ database/cart.go
@@ -14,9 +14,9 @@ import (
 var (
 	ErrCantFindProduct    = errors.New("can't find the product")
 	ErrCantDecodeProducts = errors.New("cant find the product")
-	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrUserIDIsNotValid   = errors.New("user is not valid")
 	ErrCantUpdateUser     = errors.New("cannot add this product to the cart")
-	ErrCantRemoveItemCart = errors.New("cannot remove this item from the cart")
+	ErrCantRemoveItem     = errors.New("cannot remove this item from the cart")
 	ErrCantGetItem        = errors.New("was unable to get the item from the cart")
 	ErrCantBuyCartItem    = errors.New(" cannot update the purchase")
 )
@@ -37,7 +37,7 @@ func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo
 	id, err := primitive.ObjectIDFromHex(userID)
 	if err != nil {
 		log.Println(err)
-		return ErrUserIdIsNotValid
+		return ErrUserIDIsNotValid
 	}
 
 	filter := bson.D{primitive.E{Key: "_id", Value: id}}
@@ -47,4 +47,20 @@ func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo
 		return ErrCantUpdateUser
 	}
 	return nil
+}
+
+func RemoveCartItem(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIDIsNotValid
+	}
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.M{"$pull": bson.M{"usercart": bson.M{"_id": productID}}}
+	_, err = userCollection.UpdateMany(ctx, filter, update)
+	if err != nil {
+		return ErrCantRemoveItem
+	}
+	return nil
+
 }
